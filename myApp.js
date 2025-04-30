0b77471ddd9cceea7c4e5cb6b93f7676e022d203
HeintzY <heintzymail@testmail.com>
2025-04-30 14:02:30 +0200

    feat: Install and Set Up Mongoose

--- myApp.js
+++ myApp.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
+let mongoose = require('mongoose'); //*1
 
+// 1:
+// Install and Set Up Mongoose
+// mongoose@^5.11.15 has been added to your project’s package.json file. First, require mongoose as mongoose in myApp.js. Next, create a .env file and add a MONGO_URI variable to it. Its value should be your MongoDB Atlas database URI. Be sure to surround the URI with single or double quotes, and remember that you can't use spaces around the = in environment variables. For example, MONGO_URI='VALUE'.
+// When you are done, connect to the database by calling the connect method within your myApp.js file by using the following syntax: 
+// mongoose.connect(<Your URI>, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
